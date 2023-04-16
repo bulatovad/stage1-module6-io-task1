b21699199e6344d5894df689e633ea95dabf3a7e
bulatovad <damira.bulatova@gmail.com>
2023-04-16 20:23:18 +0600

    solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,61 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file)  {
+        String profileData = getDataFromTxt(file);
+
+        String[] lines = profileData.split("\r\n");
+        Profile p = new Profile();
+        for(String line: lines) {
+            String[] data = line.split(": ");
+            switch (data[0]) {
+                case "Name":
+                    p.setName(data[1]);
+                    break;
+                case "Age":
+                    p.setAge(Integer.parseInt(data[1]));
+                    break;
+                case "Email":
+                    p.setEmail(data[1]);
+                    break;
+                case "Phone":
+                    p.setPhone(Long.parseLong(data[1]));
+                    break;
+            }
+        }
+        return p;
+    }
+
+    private String getDataFromTxt (File file)  {
+        java.io.FileReader inputStream = null;
+        String profileTxt = "";
+
+        try {
+            inputStream = new java.io.FileReader(file);
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                profileTxt += ""+ (char)c;
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+        finally {
+            if(inputStream!=null) {
+                try {
+                    inputStream.close();
+                } catch (IOException io) {
+                    io.printStackTrace();
+                }
+
+            }
+        }
+
+        return profileTxt;
     }
 }
